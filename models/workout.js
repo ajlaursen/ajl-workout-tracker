f4e30c312472fccb74aafc396ff1262259bb3443
Alex Laursen <ajlaursen@aol.com>
2021-01-19 16:39:09 -0500

    finished server file and started schemas and routes

---
+++ models/workout.js
@@ -0,0 +1,22 @@
+const mongoose =  require("mongoose")
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+    day : {
+        type: Date, 
+        default: Date.now,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name:{
+                // 
+            }
+        }
+    ]
+
+}
--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require("express")
+const mongoose = require("mongoose")
 const uri = process.env.MONGODB_URI;
+
+const PORT = process.env.PORT || 3000;
+
+const app = express(); 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(uri || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  });
+
+//   app.use(require("./routes/api.js"));
+  app.use(require("./routes/htmlRoutes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
