9886d8bd097b3b7d4f7e9a1b73ce75a2e801eaaa
Alex Laursen <ajlaursen@aol.com>
2021-01-22 00:29:42 -0500

    finished modesl working on api routes

--- models/workout.js
+++ models/workout.js
@@ -14,9 +14,29 @@ const workoutSchema = new Schema({
                 trim: true,
             },
             name:{
-                // 
-            }
+                 type: String,
+                 trim:true,
+            },
+            duration:{
+                type: Number,
+            },
+            distance:{
+                type: Number,
+            },
+            weight:{
+                type: Number,
+            },
+            reps:{
+                type: Number,
+            },
+            sets:{
+                type: Number,
+            },
         }
     ]
 
-}
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
